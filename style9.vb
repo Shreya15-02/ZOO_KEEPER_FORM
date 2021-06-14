e89ab4b22e1611a1bcdd4c9295105d79464a244a
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-15 02:27:58 +0530

    Add files via upload

---
+++ style9.vb
@@ -0,0 +1,31 @@
+.container
+{
+    height:680px;
+    width:800px;
+    margin:auto;
+    background-attachment:scroll;
+}
+fieldset
+{
+    background-color: #fff;
+    border:1px solid #ccc;
+}
+input[type="text"],
+input[type="email"],
+input[type="number"],
+input[type="gender"],
+select,
+textarea
+{
+    width=100%;
+    border:1px solid black;
+    border-radius:5px;
+    padding: 10px 15px;
+    margin:  10px 15px;
+    box-sizing: border-box;
+    
+}
+.boldd
+{
+    font-weight: bold;
+}
